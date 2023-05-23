8e37eabbb069d6c037b7d713dbbaf1205ed87994
Zero-M-creator <98219919+Zero-M-creator@users.noreply.github.com>
2023-05-23 19:43:26 +0900

    Add files via upload

---
+++ HyeangAn/spacein/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into client (client_id, client_secret, redirect_uri)
+values ('spacein', 'password', 'http://localhost:8080/callback');
---
+++ HyeangAn/spacein/src/test/java/teamCCKLJ/spacein/SpaceinApplicationTests.java
@@ -0,0 +1,13 @@
+package teamCCKLJ.spacein;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
