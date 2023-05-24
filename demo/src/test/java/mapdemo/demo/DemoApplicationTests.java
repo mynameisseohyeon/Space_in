495d8e85d27a8ec54f0e622d1a0749d90ccc8bf9
yuntime <132540142+yuntime@users.noreply.github.com>
2023-05-24 10:03:01 +0900

    Add files via upload

---
+++ demo/src/test/java/mapdemo/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package mapdemo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
